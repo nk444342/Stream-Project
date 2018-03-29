5d5f31b7397abcb591de33dd818c70c3da41c92e
ramesh <ramesh@gmail.com>
2018-03-29 13:21:38 +0530

    stream commit

---
+++ case.sh
@@ -0,0 +1,10 @@
+#! /bin/bash
+read -p 'Enter the OS name: ' name
+case $name in
+		linux)
+			cat /etc/issue
+			;;
+		unix)
+			uname -a ;;
+esac
+
---
+++ if.sh
@@ -0,0 +1,11 @@
+#! /bin/bash
+var1=10
+var2=20
+
+if [ $var1 -gt $var2 ]; then
+echo "success"
+
+else
+echo "$var1 is not greater than $var2"
+fi
+
